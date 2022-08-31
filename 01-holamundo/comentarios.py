eb0fa08ede9fcd6b4c40586caccb831b0743b5ce
Camilo Pinzon <capinzon98@gmail.com>
2022-08-31 16:42:31 -0500

    Primeros pasos con Python

---
+++ 01-holamundo/comentarios.py
@@ -0,0 +1,13 @@
+# Esto me mostrará el texto Hola por la consola
+print('Hola')
+
+# Estas dos instrucciones muestran el texto por pantalla
+print('Mundo')
+# print('!¡')
+
+"""
+Esto no se mostrará
+print("Esto no se va a mostrar")
+print("porque estará dentro de un comentario multilinea")
+"""
+print("Adiós!!")
---
+++ 01-holamundo/holamundo.py
@@ -0,0 +1,3 @@
+print("2025")
+print("Hola Mundo !! Soy Camilo Pinzón")
+print("#############")
